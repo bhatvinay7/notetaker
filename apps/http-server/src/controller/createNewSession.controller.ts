2df69016b1d87633c0aab8fc2673a7e1acbb6a87
vinay bhat <bhatvinay74@gmail.com>
2025-11-12 23:53:14 +0530

    add the session contriller

--- apps/http-server/src/controller/createNewSession.controller.ts
+++ apps/http-server/src/controller/createNewSession.controller.ts
@@ -1,14 +1,15 @@
 import {Context} from 'hono'
 import prisma from 'prisma'
 import dotenv from 'dotenv'
+import { userCredentials } from 'types'
 dotenv.config();
+import { JwtPayload } from "jsonwebtoken";
 const host=process.env.NEXT_PUBLIC_FRONTEND_URL!
-const createSession=async(c:Context){
+const createSession=async(c:Context)=>{
     try{
-        const user=c.get("user")
-        if(!user){
-            c.status(401)
-            return createSession.json({message:"authenticated"})
+        const user=c.get("user") as  userCredentials
+        if(!user.userId){
+            return c.json({message:"authenticated"},401)
         }
         const newSession=await prisma.noteSession.create({
             data:{
@@ -16,12 +17,11 @@ const createSession=async(c:Context){
                 userId:user.userId
             }
         })
-    c.status(302)
-    c.redirect(`${host}/n/${newSession.id}`)
+    return c.json({url:`${host}/dashboard/n/${newSession.id}`},201)
     }
     catch(error:any){
-        c.status(500)
-        return c.json({message:"Unexpected Error Occured"})
+        console.log(error)
+        return c.json({message:"Unexpected Error Occured"},500)
     }
 }
 
